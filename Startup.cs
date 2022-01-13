f21014111b22d809ab1d0123497d0b63ba745c81
Polina <polina.bystrevskaya@gmail.com>
2022-01-13 18:31:46 +0700

    add logging

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,8 @@ namespace lab1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+ 
+            ConfigureLogger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,5 +45,15 @@ namespace lab1
             app.UseHttpsRedirection();
             app.UseMvc();
         }
+        private void ConfigureLogger()
+       {
+           var log = new LoggerConfiguration()
+               .WriteTo.Console()
+               .WriteTo.File("logs\\lab.log", rollingInterval: RollingInterval.Day)
+               .CreateLogger();
+ 
+           Log.Logger = log;
+       }
+
     }
 }
