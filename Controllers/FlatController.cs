25c7f3bd3d7344fee3c8a2450f5ba8975b427713
Polina <polina.bystrevskaya@gmail.com>
2022-01-13 19:23:17 +0700

    bug fix

--- Controllers/FlatController.cs
+++ Controllers/FlatController.cs
@@ -7,25 +7,20 @@ namespace lab1.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class LabController : ControllerBase
+    public class Lab1Controller : ControllerBase
     {
-        private IStorage<Flat> _memCache;
-
-        public LabController(IStorage<Flat> memCache)
-        {
-            _memCache = memCache;
-        }
+        private static List<Flat> _memCache = new List<Flat>();
 
         [HttpGet]
         public ActionResult<IEnumerable<Flat>> Get()
         {
-            return Ok(_memCache.All);
+            return Ok(_memCache);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Flat> Get(Guid id)
+        public ActionResult<Flat> Get(int id)
         {
-            if (!_memCache.Has(id)) return NotFound("No such");
+            if (_memCache.Count <= id) throw new IndexOutOfRangeException("Запись не найдена");
 
             return Ok(_memCache[id]);
         }
@@ -34,38 +29,35 @@ namespace lab1.Controllers
         public IActionResult Post([FromBody] Flat value)
         {
             var validationResult = value.Validate();
-
             if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
-
             _memCache.Add(value);
-
             return Ok($"{value.ToString()} has been added");
         }
 
         [HttpPut("{id}")]
-        public IActionResult Put(Guid id, [FromBody] Flat value)
+        public IActionResult Put(int id, [FromBody] Flat value)
         {
-            if (!_memCache.Has(id)) return NotFound("No such");
+            if (_memCache.Count <= id) return NotFound("No such");
 
-            var validationResult = value.Validate();
+           var validationResult = value.Validate();
 
-            if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
+           if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
 
-            var previousValue = _memCache[id];
-            _memCache[id] = value;
+           var previousValue = _memCache[id];
+           _memCache[id] = value;
 
-            return Ok($"{previousValue.ToString()} has been updated to {value.ToString()}");
+           return Ok($"{previousValue.ToString()} has been updated to {value.ToString()}");
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(Guid id)
+        public IActionResult Delete(int id)
         {
-            if (!_memCache.Has(id)) return NotFound("No such");
+            if (_memCache.Count <= id) return NotFound("No such");
 
-            var valueToRemove = _memCache[id];
-            _memCache.RemoveAt(id);
+           var valueToRemove = _memCache[id];
+           _memCache.RemoveAt(id);
 
-            return Ok($"{valueToRemove.ToString()} has been removed");
+           return Ok($"{valueToRemove.ToString()} has been removed");
         }
     }
 }
